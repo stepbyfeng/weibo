3fd58d0222f035cf87c5a0c0fd36ba20246c13b7
mapleleaf <598236556@qq.com>
2020-01-06 14:19:14 +0000

    用户显示页面

--- app/Models/User.php
+++ app/Models/User.php
@@ -36,4 +36,14 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    /**
+     * [gravatar 生成用户头像]
+     * @param  string $size [description]
+     * @return [type]       [description]
+     */
+    public function gravatar($size = '100'){
+        $hash = md5(strtolower(trim($this->attributes['email'])));
+        return "http://www.gravatar.com/avatar/$hash?s=$size";
+    }
 }
