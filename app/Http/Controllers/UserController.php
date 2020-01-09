edec8bc78aa29bbec9174c409affb659f1c29540
mapleleaf <598236556@qq.com>
2020-01-09 08:13:16 +0000

    用户激活

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -5,13 +5,14 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\User;
 use Auth;
+use Mail;
 
 class UserController extends Controller
 {
     public function __construct()
     {
         $this->middleware('auth', [
-            'except' => ['show','create','store','index']
+            'except' => ['show', 'create', 'store', 'index', 'confirmEmail']
         ]);
 
         $this->middleware('guest', [
@@ -44,10 +45,10 @@ class UserController extends Controller
             'password' => bcrypt($request->password)
         ]);
 
-        Auth::login($user);
-        session()->flash('success','欢迎，您将这里开启一段新的旅程');
+        $this->sendEmailConfirmactionTo($user);
+        session()->flash('success','验证邮件已发送到你的注册邮箱上，请注意查收。');
 
-        return redirect()->route('users.show',[$user]);
+        return redirect('/');
     }
 
     public function edit(User $user)
@@ -89,5 +90,32 @@ class UserController extends Controller
         $user->delete();
         session()->flash('success', '成功删除用户！');
         return back();
+    }
+
+    public function sendEmailConfirmactionTo($user)
+    {
+        $view = 'emails.confirm';
+        $data = compact('user');
+        $from = '598236556@qq.com';
+        $name = 'Mapleleaf';
+        $to = $user->email;
+        $subject = '感谢注册 Weibo 应用！请确认你的邮箱。';
+
+        Mail::send($view, $data, function ($message) use ($from, $name, $to, $subject) {
+            $message->from($from, $name)->to($to)->subject($subject);
+        });
+    }
+
+    public function confirmEmail($token)
+    {
+        $user = User::where('activation_token', $token)->firstOrFail();
+
+        $user->activated = true;
+        $user->activation_token = null;
+        $user->save();
+
+        Auth::login($user);
+        session()->flash('success','恭喜你，激活成功！');
+        return redirect()->route('users.show', [$user]);
     }
 }
