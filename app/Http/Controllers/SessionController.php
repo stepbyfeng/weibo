e6f8d159c1c327c60c0da5ba6173a321048925be
mapleleaf <598236556@qq.com>
2020-01-09 06:33:42 +0000

    访问策略

--- app/Http/Controllers/SessionController.php
+++ app/Http/Controllers/SessionController.php
@@ -7,6 +7,12 @@ use Auth;
 
 class SessionController extends Controller
 {
+    public function __construct(){
+        $this->middleware('guest' , [
+            'only' => ['create']
+        ]);
+    }
+
     public function create()
     {
         return view('sessions.create');
@@ -22,7 +28,8 @@ class SessionController extends Controller
         if(Auth::attempt($credentials, $request->has('remember'))){
             // 登录成功后的相关操作
             session()->flash('success','欢迎回来！');
-            return redirect()->route('users.show',[Auth::user()]);
+            $fallback = route('users.show',Auth::user());
+            return redirect()->intended($fallback);
         }else{
             // 登录失败后的相关操作
             session()->flash('danger','很抱歉，您的邮箱和密码不匹配');
