400e986ede3efcc7c21039fd441c926c40e6fb6e
mapleleaf <598236556@qq.com>
2020-01-10 10:21:18 +0000

    社交统计信息

--- resources/views/users/show.blade.php
+++ resources/views/users/show.blade.php
@@ -8,7 +8,11 @@
             <div class="col-md-12">
                 <div class="offset-md-2 col-md-8">
                     <section class="user_info">
-                        @include('shared._user_info',['user' => $user])
+                        @include('shared._user_info', ['user' => $user])
+                    </section>
+
+                    <section class="stats mt-2">
+                        @include('shared._stats', ['user' => $user])
                     </section>
 
                     <section class="status">
